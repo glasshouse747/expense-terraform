06f68d612694111ecb5bff73eaf51521a2708e7e
Richard Okoro <glasshouse747@gmail.com>
2023-10-02 23:03:51 +0100

    Expense application commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -39,3 +39,16 @@ resource "aws_eip" "ngw" {
     Name = "${var.env}-ngw"
   }
 }
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "${var.env}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
