c1fe8e856b9e0f042511900ef47bc0c9feefc795
Richard Okoro <glasshouse747@gmail.com>
2023-10-02 23:06:27 +0100

    Expense application commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -47,8 +47,4 @@ resource "aws_nat_gateway" "ngw" {
   tags = {
     Name = "${var.env}-ngw"
   }
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.igw]
 }
