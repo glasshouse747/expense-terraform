36880e413f144eef06da7358da4b323eb5858e6a
Richard Okoro <glasshouse747@gmail.com>
2023-10-03 01:56:12 +0100

    Expense application commit

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -4,24 +4,25 @@ resource "aws_security_group" "security_group" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [var.alb_sg_allow_cidr]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.alb_sg_allow_cidr]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "${var.env}-${var.alb_type}-sg"
   }
 }
+
 resource "aws_lb" "alb" {
   name               = "${var.env}-${var.alb_type}"
   internal           = var.internal
@@ -32,3 +33,4 @@ resource "aws_lb" "alb" {
     Environment = "${var.env}-${var.alb_type}"
   }
 }
+
